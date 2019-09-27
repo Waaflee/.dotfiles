62212849cf44a56e9e24b5415e65a0a903cd80ad
Waaflee <agustinwafle@gmail.com>
2019-09-27 13:20:01 -0300

    Modularized bars and hostname based init

--- bspwm/.config/compton/launch.sh
+++ bspwm/.config/compton/launch.sh
@@ -7,6 +7,12 @@ killall -q compton
 while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
 
 # Launch compton in background, using default config location ~/.config/compton/compton.conf
-compton -b --config $HOME/.config/compton/compton.conf
+if [[ $(hostname) == 'archlinux' ]]; then
+    compton -b --config $HOME/.config/compton/compton.conf
+elif [[ $(hostname) == 'WafleBook' ]]; then
+    compton -b --config $HOME/.config/compton/compton.conf --no-dock-shadow --blur-background-exclude "window_type = 'dock'" --opacity-rule "class_g = 'Polybar'"
+else
+    compton -b --config $HOME/.config/compton/compton.conf
+fi
 
 echo "Compton launched..."
--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -8,6 +8,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export Monitor=$(polybar --list-monitors | cut -d":" -f1)
 # Launch Polybar, using default config location ~/.config/polybar/config
 if [[ $(hostname) == 'archlinux' ]]; then
     polybar tiled &
