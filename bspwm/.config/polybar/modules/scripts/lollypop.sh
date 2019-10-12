e4af1c3cf7fa70fbc859e49ac583355e307f4100
Waaflee <agustinwafle@gmail.com>
2019-10-12 15:05:17 -0300

    Removed lollypop module

--- bspwm/.config/polybar/modules/scripts/lollypop.sh
+++ bspwm/.config/polybar/modules/scripts/lollypop.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-artist=$(playerctl metadata | grep artist)
-title=$(playerctl metadata | grep title)
-echo "  ${title:35}:  ${artist:35}"
+SERVICE='lollypop'
+ 
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    status=$(playerctl --player=Lollypop status | grep Playing)
+    artist=$(playerctl --player=Lollypop metadata | grep artist)
+    title=$(playerctl --player=Lollypop metadata | grep title)
+    if [[ "$status" = "Playing" ]]; then
+        echo "  ${artist:34}:  ${title:34}"
+    elif [[ "$status" = "Paused" ]]; then
+        echo "  ${artist:34}:  ${title:34}"
+    fi
+else
+    echo ""
+fi
