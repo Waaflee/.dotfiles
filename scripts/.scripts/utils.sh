7ee53736d946b5f029bf2a95f81b2fe1e3e2eed2
Waaflee <agustinwafle@gmail.com>
2019-12-10 16:15:15 -0300

    aded blockify commands

--- scripts/.scripts/utils.sh
+++ scripts/.scripts/utils.sh
@@ -14,3 +14,34 @@ hidesktop() {
     cp "$@" ~/.local/share/applications/$(basename "$@")
     echo "NoDisplay=true" >> ~/.local/share/applications/$(basename "$@")
 }
+
+bb() {
+    local signal
+    case "$1" in
+        '')  blockify-dbus get 2>/dev/null && return 0;;
+        ex|exit)
+            signal='TERM';;       # Exit
+        b|block)
+            signal='USR1';;       # Block
+        u|unblock)
+            signal='USR2';;       # Unblock
+        p|previous)
+            signal='RTMIN';;      # Previous song
+        n|next)
+            signal='RTMIN+1';;    # Next song
+        t|toggle)
+            signal='RTMIN+2';;    # Toggle play song
+        tb|toggleblock)
+            signal='RTMIN+3';;    # Toggle block song
+        ip|iprevious)
+            signal='RTMIN+10';;   # Previous interlude song
+        in|inext)
+            signal='RTMIN+11';;   # Next interlude song
+        it|itoggle)
+            signal='RTMIN+12';;   # Toggle play interlude song
+        itr|itoggleresume)
+            signal='RTMIN+13';;   # Toggle interlude resume
+        *) echo "Usage: bb ( b[lock] | u[nblock] | p[revious] | n[ext] | t[oggle] | t[oggle]b[lock] |...)" && return 0;;
+    esac
+    pkill --signal "$signal" -f 'python.*blockify'
+}
